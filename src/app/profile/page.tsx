8334fbf40cfbc1be08e629ec34fcd60b09fb0df2
to.ino <theo.hinaut@student.junia.com>
2025-02-13 15:11:58 +0100

    CHORE Architecture

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+
+
+const Profile = () => {
+  return (
+    <div>
+
+      <h1>Profile</h1>
+      <p>Voici votre profil personnel. Modifiez vos informations ici.</p>
+    </div>
+  )
+}
+
+export default Profile
