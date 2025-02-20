379842f5a452ff0f1adf060766ab6da7211f7d32
Thomas Lefebvre <thomaslfb3@gmail.com>
2025-02-20 15:40:46 +0100

    fix: next public url

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,5 @@
 export const registerUser = async (email, password) => {
-    const response = await fetch(`${process.env.BACKEND_URL}/auth/register`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -16,7 +16,7 @@ export const registerUser = async (email, password) => {
   };
   
   export const loginUser = async (email, password) => {
-    const response = await fetch(`${process.env.BACKEND_URL}/auth/login`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
